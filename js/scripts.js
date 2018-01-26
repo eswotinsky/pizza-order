4e3985a4ef9e59826e476674f73b95ed70b8b462
Eric Swotinsky <eric.swotinsky@gmail.com>
2018-01-26 09:44:37 -0800

    add price prototype; create new Pizza object on order confirmation.

--- js/scripts.js
+++ js/scripts.js
@@ -6,23 +6,45 @@ function Pizza(toppings, size) {
 }
 
 Pizza.prototype.price = function() {
+  var price = 0;
+  price += (this.toppings * 1.5);
 
+  switch (this.size) {
+    case "small":
+      price += 5;
+      break;
+    case "medium":
+      price += 10;
+      break;
+    default:
+      price += 0;
+      break;
+  }
+  return price;
 }
 
 
 
 //front end
-$(function) {
+$(function() {
 
-  $("#orderPizza").submit(function(event){
-    event.preventDefault();
+  $("#orderPizza").click(function(){
 
-    var orderToppings = $("").val();
-    var orderSize = $("").val();
 
-    var myPizza = new Pizza();
+    var orderToppings = $("input[name=toppings]:checked").length;
+    console.log(orderToppings);
+
+    var orderSize = $("input[name=size]:checked").val();
+    console.log(orderSize);
+
+
+    var myPizza = new Pizza(orderToppings, orderSize);
+    console.log(myPizza);
+
+    var pizzaPrice = myPizza.price();
+    console.log(pizzaPrice);
 
   })
 
 
-}
+})
